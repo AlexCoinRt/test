fd928e5a6e4f2becb70219dc4a155067f9174217
AlexCoinRt <64779334+AlexCoinRt@users.noreply.github.com>
2020-05-05 21:39:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+<<<<<<< HEAD
+    return "Hello World! dfgfgdfgfgdfg"
+=======
+    return "Hello World! 111111111111111111111"
+>>>>>>> 448fd89b1facbea2f32f9f8542d6425f7b71b849
+
+#if __name__ == "__main__":
+#   app.run()
